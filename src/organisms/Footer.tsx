355f0b842f8f1d5a85a4b45be424d199d76465e3
Anojan Pirabakaran <69903847+anojanpirabakaran@users.noreply.github.com>
2023-05-09 12:25:32 +0200

    add whole project

---
+++ src/organisms/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./Footer.css"
+export default function Footer() {
+    return (
+        <div className="wrapper">
+          <div className="content">
+           
+          </div>
+          <div className="footer"><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons Lizenzvertrag" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />Dieses Werk ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Namensnennung - Nicht kommerziell - Keine Bearbeitungen 4.0 International Lizenz</a></div>
+        </div>
+      );
+}
